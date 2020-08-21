795d5a5194be99f77e7262181e1ea26e9444458e
mcnkowski <mcnkowski@gmail.com>
2020-08-21 19:23:36 +0200

    minor changes

--- src/main/scala/TimedTextParser.scala
+++ src/main/scala/TimedTextParser.scala
@@ -5,16 +5,16 @@ import scala.util.Try
 import javax.xml.parsers.SAXParserFactory
 
 class TimedTextParser extends CaptionParser {
-  val spf = SAXParserFactory.newInstance()
+  private val spf = SAXParserFactory.newInstance()
   spf.setFeature("http://xml.org/sax/features/external-general-entities", false)
   spf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true)
-  val saxParser = spf.newSAXParser()
+  private val saxParser = spf.newSAXParser()
   
   def parse (input:String):String = {
-    val trial = Try {
+    Try {
       val xml = XML.withSAXParser(saxParser).loadString(input)
       (xml \ "text").map(text => text.text).toSeq.mkString(" ")
-    }
-    trial.getOrElse("").replaceAll("\n+"," ")
+    }.getOrElse("").replaceAll("\n+"," ")
   }
+
 }
